1810c91378fe317b255bd8008d432f165b1bd87d
Murat YAPICI <45922313+yapicimurat@users.noreply.github.com>
2022-01-08 15:46:53 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import {io} from "socket.io-client";
 
 const socket = io("https://muratyapici.herokuapp.com/");
 let nickname = "";
-console.log("uygulama baslayacak insallah");
+console.log("uygulama baslayacak insallah v2");
 socket.on("connect",() => {
-  nickname = prompt("Enter a nickname");
-  ReactDOM.render(
-    <React.StrictMode>
-      <MessageList/>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+  
+ 
   
 });
+nickname = prompt("Enter a nickname");
+ReactDOM.render(
+  <React.StrictMode>
+    <MessageList/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 export {socket, nickname};
