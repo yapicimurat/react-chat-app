f32567b2a8a071944a37dc037293961a0743e36e
Murat YAPICI <45922313+yapicimurat@users.noreply.github.com>
2022-01-08 15:38:51 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import MessageList from './MessageList';
 import {io} from "socket.io-client";
 
-const socket = io("https://muratyapici.herokuapp.com");
+const socket = io("https://muratyapici.herokuapp.com/");
 let nickname = "";
+console.log("uygulama baslayacak insallah");
 socket.on("connect",() => {
   nickname = prompt("Enter a nickname");
   ReactDOM.render(
